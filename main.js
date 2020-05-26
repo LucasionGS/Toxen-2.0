a3398bccc5a5b02e8c5deecf9c1c9c5bc0282ccb
Lucasion <lucasion@hotmail.com>
2020-05-26 14:56:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ const winHeight = 768;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
-// let usageWin;
-
+app.allowRendererProcessReuse = true; // Electron mad if i don't :(
 
 function createWindow () {
   // Create the browser window.
